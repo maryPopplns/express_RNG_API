612234e0f006ae418448ab11e339c00e12e5f8a9
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-05 11:21:04 -0700

    random number being created for home route

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const max = 1500;
+  const min = 0;
+  const RANDOM = Math.random() * (max - min) + min;
+  res.send(RANDOM);
 });
 
 app.listen(port, () => {
